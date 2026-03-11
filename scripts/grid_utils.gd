b684ca2a68467135766c4f9a88d86621c6380083
Amando Cuenca Carabia <amandodaw@gmail.com>
2026-03-11 12:49:20 +0100

    Añadido humano y movimiento por grid basico. Creada clase grid_utils para datos relacionados con el grid

---
+++ scripts/grid_utils.gd
@@ -0,0 +1,19 @@
+class_name GridUtils
+
+const TILE_SIZE : int = 16
+
+const grid_offset : Vector2i = Vector2i(TILE_SIZE/2, TILE_SIZE/2)
+
+enum direction {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
+
+const DIR = [
+	Vector2i(0, -1),
+	Vector2i(0, 1),
+	Vector2i(1, 0),
+	Vector2i(-1, 0)
+]
--- scripts/human.gd
+++ scripts/human.gd
@@ -1,1 +1,15 @@
 extends Sprite2D
+
+var target_pos : Vector2i = Vector2i.ZERO
+var grid_pos : Vector2i = Vector2i.ZERO
+
+var speed : int = 100
+
+
+func move_to(dir : GridUtils.direction) -> void:
+	grid_pos += GridUtils.DIR[dir]
+	position = grid_pos * GridUtils.TILE_SIZE + GridUtils.grid_offset
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		move_to(GridUtils.direction.LEFT)
